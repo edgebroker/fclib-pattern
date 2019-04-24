d8ee7c899cfb83cfb305b566fa524ba7de8d8353
iitsoftware <am@iit.de>
2019-04-24 16:30:22 +0200

    flush & bulk to reduce events

--- Pattern/Management/Subscription Forwarder/init/handler.js
+++ Pattern/Management/Subscription Forwarder/init/handler.js
@@ -1,25 +1,47 @@
 function handler() {
     var self = this;
-    stream.create().memory("subscriptions").heap().createIndex("id").createIndex("context").createIndex("channelid");
+    stream.create().memory(this.compid + "-subscriptions").heap().createIndex("id").createIndex("context").createIndex("channelid");
+    stream.create().timer(this.compid + "-checklimit").interval().milliseconds(this.props["flushinterval"]).onTimer(function (timer) {
+        stream.memory(self.compid + "-subscriptions").forEach(function (subscription) {
+            var mem = stream.memory(subscription.property("id").value().toString());
+            if (mem != null)
+                mem.checkLimit();
+        })
+    });
 
     this.addSubscription = function (context, channelid) {
         var id = context + "-" + channelid;
-        var inputs = stream.memory("subscriptions").index("id").get(id);
+        var inputs = stream.memory(this.compid + "-subscriptions").index("id").get(id);
         if (inputs.size() === 0) {
+            stream.create().memory(id).heap()
+                .limit()
+                .count(500)
+                .limit()
+                .time()
+                .milliseconds(self.props["flushinterval"])
+                .tumbling()
+                .onRetire(function (retired) {
+                    flush(channelid, retired);
+                });
             stream.create().input(id).management().context(context)
                 .onAdd(function (input) {
-                    sendToChannel(channelid, input.current());
+                    if (!isFiltered(input.current()))
+                        sendToChannel(channelid, input.current());
                 })
                 .onChange(function (input) {
-                    sendToChannel(channelid, input.current());
+                    if (isLiveData(input.current()))
+                        stream.memory(id).add(input.current());
+                    else
+                        sendToChannel(channelid, input.current());
                 })
                 .onRemove(function (input) {
-                    sendToChannel(channelid, input.current());
+                    if (!isFiltered(input.current()))
+                        sendToChannel(channelid, input.current());
                 }).start();
-            var outputs = stream.memory("subscriptions").index("channelid").get(channelid);
+            var outputs = stream.memory(this.compid + "-subscriptions").index("channelid").get(channelid);
             if (outputs.size() === 0)
                 stream.create().output(channelid).queue();
-            stream.memory("subscriptions").add(
+            stream.memory(this.compid + "-subscriptions").add(
                 stream.create().message().message()
                     .property("id").set(id)
                     .property("context").set(context)
@@ -30,14 +52,59 @@ function handler() {
 
     this.removeSubscription = function (context, channelid) {
         var id = context + "-" + channelid;
-        if (stream.memory("subscriptions").index("id").get(id).size() > 0) {
-            stream.memory("subscriptions").index("id").remove(id);
+        if (stream.memory(this.compid + "-subscriptions").index("id").get(id).size() > 0) {
+            stream.memory(this.compid + "-subscriptions").index("id").remove(id);
             stream.input(id).close();
-            var outputs = stream.memory("subscriptions").index("channelid").get(channelid);
+            var outputs = stream.memory(this.compid + "-subscriptions").index("channelid").get(channelid);
             if (outputs.size() === 0)
                 stream.output(channelid).close();
+            stream.memory(id).close();
         }
     };
+
+    function isFiltered(message) {
+        var ctx = message.property("_CTX").value().toString();
+        var name = message.property("name").value().toString();
+        return ctx === "/sys$queuemanager/usage" && name.startsWith("tpc$");
+    }
+
+    function isLiveData(message) {
+        var ctx = message.property("_CTX").value().toString();
+        return ctx.indexOf("/usage") !== -1;
+    }
+
+    function flush(channelid, memory) {
+        var bulk = [];
+        memory.group("name").forEach(function (groupmem) {
+            var event = groupmem.first();
+            groupmem.forEach(function (msg) {
+                msg.properties().forEach(function (prop) {
+                    var name = prop.name();
+                    var value = prop.value().toObject();
+                    if (!(name === "_CTX" || name === "_TIME" || name === "_OP"))
+                        event.property(name).set(value);
+                })
+            });
+            var body = {};
+            event.properties().forEach(function (p) {
+                body[p.name()] = p.value().toObject();
+            });
+            var content = JSON.stringify(body);
+            bulk.push(content);
+        });
+        sendBulk(channelid, bulk);
+    }
+
+    function sendBulk(channelid, bulk) {
+        var body = {
+            _op: "bulk",
+            _content: bulk
+        };
+        var msg = stream.create().message().textMessage().nonpersistent().property("channelid").set(channelid).body(JSON.stringify(body));
+        self.executeOutputLink("Debug", msg);
+        if (stream.output(channelid) != null)
+            stream.output(channelid).send(msg);
+    }
 
     function sendToChannel(channelid, message) {
         var body = {};
