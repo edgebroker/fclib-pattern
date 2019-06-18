53d0583d2570b04683b9d2b748f36592047886ab
Mike <reach@sysdevmike.com>
2019-06-18 21:58:02 +0900

    Update 'New and Set Properties'.

--- Pattern/Message/New and Set Properties/links/input/In/handler.js
+++ Pattern/Message/New and Set Properties/links/input/In/handler.js
@@ -3,7 +3,12 @@ function handler(In) {
     var outMsg = stream.create().message().message();
 
     for (var i=0;i<this.properties.length;i++){
-        outMsg.property(this.properties[i]).set(convert(subSystemTags(subRefProps(In, this.flowcontext.substitute(this.values[i]))), this.types[i]));
+
+        var name = this.properties[i]["name"];
+        var type = this.properties[i]["type"];
+        var value = this.properties[i]["value"];
+
+        outMsg.property(name).set(convert(subSystemTags(subRefProps(In, this.flowcontext.substitute(value))), type));
     }
 
     this.executeOutputLink("Out", outMsg);
