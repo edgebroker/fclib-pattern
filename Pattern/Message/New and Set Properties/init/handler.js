74b58b6005779ecb4bed653645572b70b6f141ff
iitsoftware <am@iit.de>
2020-03-25 11:21:39 +0100

    typeconvert added

--- Pattern/Message/New and Set Properties/init/handler.js
+++ Pattern/Message/New and Set Properties/init/handler.js
@@ -2,13 +2,6 @@ function handler() {
     this.properties = this.props["properties"];
     this.types = this.props["types"];
     this.values = this.props["values"];
-    this.BOOLEAN = Java.type("java.lang.Boolean");
-    this.INTEGER = Java.type("java.lang.Integer");
-    this.LONG = Java.type("java.lang.Long");
-    this.DOUBLE = Java.type("java.lang.Double");
-    this.FLOAT = Java.type("java.lang.Float");
-
-
 
     for (var i=0;i<this.properties.length;i++) {
         var type = this.properties[i]["type"];
